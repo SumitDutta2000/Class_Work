7d35a7aa5a9ddf02abfd06d24f96ec02c0e6bdc3
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-07-15 11:18:33 +0530

    Add files via upload

---
+++ NestedForloop.java
@@ -0,0 +1,16 @@
+package basicprogram;
+
+public class NestedForloop {
+	public static void main(String[] args) {
+		int i,j;
+		for(i=1;i<4;i++)
+		{
+			for(j=1;j<3;j++)
+			{
+				System.out.print("Hello   ");
+			}
+			System.out.println( );
+		}
+	}
+
+}
