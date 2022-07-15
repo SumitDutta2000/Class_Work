eb48a8dd70e0f795bd4ceecc7d603951867bc1f1
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-07-15 11:20:43 +0530

    Add files via upload

---
+++ StrongNumber.java
@@ -0,0 +1,23 @@
+package basicprogram;
+import java.util.Scanner;
+public class StrongNumber {
+	public static void main(String[] args) {
+		int i,num,sum=0,temp,rem,fact;
+		Scanner sc=new Scanner(System.in);
+				System.out.println(" Enter the number....");
+				num=sc.nextInt();
+				
+	    temp=num;//store actual variable for future use
+				
+		while(num!=0) 
+		{
+			 rem=num%10;//taking the last number
+			 fact=1;
+			
+			
+		}
+		
+		
+	}
+
+}
